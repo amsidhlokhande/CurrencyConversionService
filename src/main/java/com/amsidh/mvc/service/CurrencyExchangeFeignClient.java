7c7abd5f644da1b9ff540346af9e29be7b9787b4
amsidhlokhande <amsidhlokhande@gmail.com>
2021-02-18 19:08:31 +0530

    Added Ingress support so changes from LoadBalancer To NodePort done

--- src/main/java/com/amsidh/mvc/service/CurrencyExchangeFeignClient.java
+++ src/main/java/com/amsidh/mvc/service/CurrencyExchangeFeignClient.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "http://${CURRENCY_EXCHANGE_HOST:localhost}:8100", configuration = CurrencyExchangeFeignClientConfiguration.class)
+//@FeignClient(name = "currency-exchange", url = "http://${CURRENCY_EXCHANGE_HOST:localhost}:8100", configuration = CurrencyExchangeFeignClientConfiguration.class)
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_URI:http://localhost}:8100", configuration = CurrencyExchangeFeignClientConfiguration.class)
 public interface CurrencyExchangeFeignClient {
 
     @GetMapping("/currency-exchange/{fromCurrency}/{toCurrency}")
